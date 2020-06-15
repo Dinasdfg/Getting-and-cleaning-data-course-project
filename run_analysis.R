965db958470d82d3cd1999c9aa3a3b61f019f583
Dinara <63246554+Dinasdfg@users.noreply.github.com>
2020-06-15 22:11:01 +0300

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,31 @@
+url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+destfile <- "./data"
+download.file(url, destfile)
+unzip("data")
+activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
+features <- read.table("./UCI HAR Dataset/features.txt")
+Xtrain <- read.table("./UCI HAR Dataset/train/X_train.txt")
+Ytrain <- read.table("./UCI HAR Dataset/train/y_train.txt")
+Strain <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+Xtest <- read.table("./UCI HAR Dataset/test/X_test.txt")
+Ytest <- read.table("./UCI HAR Dataset/test/y_test.txt")
+Stest <- read.table("./UCI HAR Dataset/test/Subject_test.txt")
+Feature <- rbind(Xtrain, Xtest)
+Activity <- rbind(Ytrain, Ytest)
+Subject <- rbind(Strain, Stest)
+colnames(Feature) <- features$V2
+colnames(Activity) <- "Activity"
+colnames(Subject) <- "Subject"
+Activity <- activity_labels[Activity[,1], 2]
+Merged <- cbind(Subject, Activity, Feature)
+Selected_variables <- grep(".*Mean.*|.*Std.*", features$V2, ignore.case=TRUE)
+Required_cols <- c(1, 2, Selected_variables)
+Extracted_data <- Merged[, Required_cols]
+names(Extracted_data)<-gsub("^t", "time", names(Extracted_data))
+names(Extracted_data)<-gsub("^f", "frequency", names(Extracted_data))
+names(Extracted_data)<-gsub("Acc", "Accelerometer", names(Extracted_data))
+names(Extracted_data)<-gsub("Gyro", "Gyroscope", names(Extracted_data))
+names(Extracted_data)<-gsub("Mag", "Magnitude", names(Extracted_data))
+names(Extracted_data)<-gsub("BodyBody", "Body", names(Extracted_data))
+Data2 <- aggregate(. ~Subject + Activity, Extracted_data, mean)
+write.table(Data2, file = "./UCI HAR Dataset/data2.txt", row.names = FALSE, col.names = TRUE)
